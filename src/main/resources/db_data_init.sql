c7a07f3155f08efc113227f1a72e04a34b8a6750
dawidkotarba <dawidkotarba>
2017-11-29 15:07:45 +0100

    update db scripts

--- src/main/resources/db_data_init.sql
+++ src/main/resources/db_data_init.sql
@@ -3,31 +3,15 @@ INSERT INTO public.users (username, password, enabled, role) VALUES
   ('admin', 'admin', TRUE, 'ROLE_ADMIN'),
   ('user', 'user', TRUE, 'ROLE_USER');
 
--- CITIES TABLE
-INSERT INTO public.cities (name, country, population) VALUES
-  ('Warsaw', 1, 1735442),
-  ('Krakow', 1, 0),
-  ('Lodz', 1, 0),
-  ('Poznan', 1, 0),
-  ('Gdansk', 1, 0),
-  ('Berlin', 2, 3440991),
-  ('London', 3, 8416535),
-  ('Amsterdam', 4, 801200),
-  ('Moscow', 5, 12100000);
+-- AUTHORS TABLE
+INSERT INTO public.authors (username, firstname, lastname) VALUES
+  ('dawidk', 'Dawid', 'K.');
 
--- COUNTRY TABLE
-INSERT INTO public.countries (name, capital, area, population, currency) VALUES
-  ('Poland', 1, 312685, 38000000, 'PLN'),
-  ('Germany', 6, 357340, 81000000, 'EUR'),
-  ('UK', 7, 244820, 63000000, 'GBP'),
-  ('Netherlands', 9, 41526, 16800000, 'EUR'),
-  ('Russia', 8, 17098242, 143500000, 'RUB');
+-- POSTS TABLE
+INSERT INTO public.posts (author, subject, body, published) VALUES
+  (1, 'Test post', 'Test post body', '2017-11-29');
 
--- NEIGHBOUR TABLE
-INSERT INTO public.neighbours (country, neighbour) VALUES
-  (1, 2),
-  (1, 5),
-  (2, 1),
-  (2, 4),
-  (4, 2),
-  (5, 1);
+-- COMMENTS TABLE
+INSERT INTO public.comments (author, subject, body, post) VALUES
+  ('Stefan', 'Test comment', 'Test comment body', 1),
+  ('Mietek', 'Test comment 2', 'Test comment body 2', 1);
