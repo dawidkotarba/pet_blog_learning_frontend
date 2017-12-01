a34ed6fff239664f4c09e205eb4692c80482989a
dawidkotarba <dawidkotarba>
2017-12-01 20:49:47 +0100

    add spock and configure unit/integration tests

---
+++ src/test/groovy/com/dawidkotarba/blog/AbstractIntegrationTest.groovy
@@ -0,0 +1,10 @@
+package com.dawidkotarba.blog
+
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import spock.lang.Specification
+
+@SpringBootTest(classes = com.dawidkotarba.blog.BlogApp.class)
+@Import([IntegrationTestMockingConfig])
+abstract class AbstractIntegrationTest extends Specification {
+}
