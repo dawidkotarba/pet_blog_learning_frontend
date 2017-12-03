8780f773f210bdaad84399d94e145a34a4e284e5
dawidkotarba <dawidkotarba>
2017-12-03 20:12:40 +0100

    update dependencies

--- src/main/java/com/dawidkotarba/blog/controllers/UserController.java
+++ src/main/java/com/dawidkotarba/blog/controllers/UserController.java
@@ -13,13 +13,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-/**
- * Created by Dawid Kotarba on 15.11.2015.
- */
-
 @RestController
 @RequestMapping(value = "/users")
 class UserController {
@@ -33,20 +26,15 @@ class UserController {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserDto> findAll() {
-        return updateResourceResults(userFacade.findAll());
+        return userFacade.findAll();
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<UserDto> getByName(@PathVariable final String name) {
         final Optional<UserDto> result = userFacade.findByName(name);
         if (result.isPresent()) {
-            return updateResourceResults(Collections.singletonList(result.get()));
+            Collections.singletonList(result.get());
         }
         return Collections.emptyList();
-    }
-
-    private List<UserDto> updateResourceResults(final List<UserDto> dtos) {
-        dtos.forEach(dto -> dto.add(linkTo(methodOn(UserController.class).getByName(dto.getUsername())).withSelfRel()));
-        return dtos;
     }
 }
