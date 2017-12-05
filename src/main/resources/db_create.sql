a5c13d9c2341420b2bee11aee8bfb3c2c0bfa57e
dawidkotarba <dawidkotarba>
2017-12-05 18:58:00 +0100

    update liquibase scripts

--- src/main/resources/db_create.sql
+++ src/main/resources/db_create.sql
@@ -1,13 +1,3 @@
--- USERS
-CREATE USER IF NOT EXISTS SA
-  SALT '6a0e04afeac6bccc' HASH '065c94fe8fb12546403bd50ab883a59cd664b25ceb9f7bd8a1420a74099d0d3b' ADMIN;
-
--- SEQUENCE
-CREATE SEQUENCE users_seq INCREMENT BY 1;
-CREATE SEQUENCE authors_seq INCREMENT BY 1;
-CREATE SEQUENCE posts_seq INCREMENT BY 1;
-CREATE SEQUENCE comments_seq INCREMENT BY 1;
-
 -- TABLES
 CREATE TABLE users (
   id INT NOT NULL DEFAULT nextval('users_seq') PRIMARY KEY,
@@ -52,22 +42,3 @@ CREATE TABLE comments (
   published DATE,
   FOREIGN KEY (post_id) REFERENCES posts(id)
 );
-
----- INDEXES
-CREATE INDEX users_index on users(id);
-CREATE INDEX authors_index on authors(id);
-CREATE INDEX posts_index on posts(id);
---
----- VIEWS
---CREATE VIEW POPULATION_DENSITY AS
---SELECT ct.name country, ct.population / ct.area population_density
---FROM countries ct JOIN cities cp ON ct.capital = cp.id
---order by population_density desc;
---
---CREATE VIEW COUNTRIES_WITHOUT_NEIGHBOURS AS
---SELECT c.name countries_wo_neighbour
---FROM countries c WHERE c.id NOT IN (SELECT country FROM neighbours);
---
---CREATE VIEW COUNTRIES_AND_CAPS AS
---SELECT ct.name countries, cp.name cities
---FROM countries ct JOIN countries cp ON ct.capital = cp.id;
