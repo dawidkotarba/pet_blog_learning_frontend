0c29bfce45bb8cb350b290f7077eb7dc7a7c08ff
Filip Domanski <filippdomanski@gmail.com>
2017-12-05 11:25:40 +0100

    change devMessage

--- src/main/java/com/dawidkotarba/blog/controllers/UserController.java
+++ src/main/java/com/dawidkotarba/blog/controllers/UserController.java
@@ -3,7 +3,6 @@ package com.dawidkotarba.blog.controllers;
 import com.dawidkotarba.blog.exceptions.NotFoundException;
 import com.dawidkotarba.blog.facade.UserFacade;
 import com.dawidkotarba.blog.model.dto.UserDto;
-import com.dawidkotarba.blog.service.i18n.LocalizationService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +18,10 @@ import java.util.Optional;
 class UserController {
 
     private final UserFacade userFacade;
-    private final LocalizationService localizationService;
 
     @Inject
-    public UserController(final UserFacade userFacade, final LocalizationService localizationService) {
+    UserController(final UserFacade userFacade) {
         this.userFacade = userFacade;
-        this.localizationService = localizationService;
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -36,6 +33,6 @@ class UserController {
     public UserDto getByName(@PathVariable final String name) {
         final Optional<UserDto> result = userFacade.findByName(name);
 
-        return result.orElseThrow(() -> new NotFoundException(localizationService.getMessage("user.not.found", new String[]{name})));
+        return result.orElseThrow(() -> new NotFoundException("User [" + name + "]" + " not found."));
     }
 }
