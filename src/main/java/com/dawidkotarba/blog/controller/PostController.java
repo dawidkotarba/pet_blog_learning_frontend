d2dcad064c8bbda832dccbcff84648b62cd54cc0
Bartlomiej Klonowski <bartlomiej.klonowski@epam.com>
2017-12-07 13:50:48 +0100

    working rest for requesting posts in given dates / month

--- src/main/java/com/dawidkotarba/blog/controller/PostController.java
+++ src/main/java/com/dawidkotarba/blog/controller/PostController.java
@@ -1,13 +1,16 @@
-package com.dawidkotarba.blog.controller;
+package com.dawidkotarba.blog.controllers;
 
 import com.dawidkotarba.blog.facade.PostFacade;
 import com.dawidkotarba.blog.model.dto.PostInDto;
 import com.dawidkotarba.blog.model.dto.PostOutDto;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -26,7 +29,7 @@ class PostController {
         return postFacade.findAll();
     }
 
-    @RequestMapping(value = "/{subject}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/subject/{subject}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PostOutDto> findBySubject(@PathVariable final String subject) {
         return postFacade.findBySubject(subject);
     }
@@ -34,5 +37,19 @@ class PostController {
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public void add(@RequestBody @Valid final PostInDto postDto) {
         postFacade.add(postDto);
+    }
+
+    @RequestMapping(value = "/search/{dayOfAMonth}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PostOutDto> findMontlyByDayOfAMonth(@RequestParam("dayOfAMonth")
+                                                 @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate dayOfAMonth) {
+        return postFacade.findMontlyByDayOfAMonth(dayOfAMonth);
+    }
+
+    @RequestMapping(value = "/search/{fromDate}/{toDate}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PostOutDto> findFromDayToDay(@RequestParam("fromDate")
+                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime fromDate,
+                                          @RequestParam("toDate")
+                                          @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime toDate) {
+        return postFacade.findFromDateToDate(fromDate, toDate);
     }
 }
--- src/main/java/com/dawidkotarba/blog/repository/PostRepository.java
+++ src/main/java/com/dawidkotarba/blog/repository/PostRepository.java
@@ -3,9 +3,12 @@ package com.dawidkotarba.blog.repository;
 import com.dawidkotarba.blog.model.entities.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
 
     List<PostEntity> findBySubject(String subject);
+
+    List<PostEntity> findByPublishedBetween(Timestamp fromDate, Timestamp toDate);
 }
