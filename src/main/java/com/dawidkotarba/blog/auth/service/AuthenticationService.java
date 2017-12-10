4302c265890c900070dd4d69302fc22ab001e445
dawidkotarba <dawidkotarba>
2017-12-10 22:31:00 +0100

    add spring security authentication - part 2

--- src/main/java/com/dawidkotarba/blog/auth/service/AuthenticationService.java
+++ src/main/java/com/dawidkotarba/blog/auth/service/AuthenticationService.java
@@ -1,15 +1,7 @@
 package com.dawidkotarba.blog.auth.service;
 
-import com.dawidkotarba.blog.auth.enums.UserAuthority;
 import org.springframework.security.core.Authentication;
-
-import java.util.Set;
 
 public interface AuthenticationService {
     Authentication authenticate(String username, String password);
-
-    Set<UserAuthority> getCurrentUserAuthorities();
-
-    boolean hasCurrentUserAuthority(UserAuthority userAuthority);
-
 }
--- src/main/java/com/dawidkotarba/blog/auth/service/AuthorizationService.java
+++ src/main/java/com/dawidkotarba/blog/auth/service/AuthorizationService.java
@@ -2,6 +2,12 @@ package com.dawidkotarba.blog.auth.service;
 
 import com.dawidkotarba.blog.auth.enums.UserAuthority;
 
+import java.util.Set;
+
 public interface AuthorizationService {
     void authorize(UserAuthority... authorizedRoles);
+
+    Set<UserAuthority> getCurrentUserAuthorities();
+
+    boolean hasCurrentUserAuthority(UserAuthority userAuthority);
 }
