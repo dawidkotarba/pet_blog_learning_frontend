997a1d7ee667efebc161eb034fe7528318b5073e
dawidkotarba <dawidkotarba>
2017-12-13 12:53:27 +0100

    update FE model

---
+++ ngblog/src/app/model/comment.ts
@@ -0,0 +1,8 @@
+export class Comment {
+  id: number;
+  author: string;
+  subject: string;
+  body: string;
+  post: number;
+  published: string;
+}
--- ngblog/src/app/model/post.ts
+++ ngblog/src/app/model/post.ts
@@ -1,6 +1,13 @@
+import {Author} from './author';
+import {Comment} from './comment';
+import {Label} from './label';
+
 export class Post {
   id: number;
   subject: string;
   body: string;
   published: string;
+  authors: Author[];
+  comments: Comment[];
+  labels: Label[];
 }
