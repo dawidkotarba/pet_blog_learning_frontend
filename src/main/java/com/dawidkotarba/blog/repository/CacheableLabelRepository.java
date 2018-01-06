3750d741adc7683816f9de07ce9c84aba98044c0
dawidkotarba <dawidkotarba>
2018-01-07 00:27:37 +0100

    fix ehcache eviction on save

--- src/main/java/com/dawidkotarba/blog/repository/CacheableLabelRepository.java
+++ src/main/java/com/dawidkotarba/blog/repository/CacheableLabelRepository.java
@@ -1,6 +1,8 @@
 package com.dawidkotarba.blog.repository;
 
 import com.dawidkotarba.blog.model.entities.impl.LabelEntity;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -8,6 +10,15 @@ import java.util.List;
 public interface CacheableLabelRepository extends BaseRepository<LabelEntity> {
 
     @Override
+    @CacheEvict(value = "labelsCache", allEntries = true)
+    <S extends LabelEntity> S save(S entity);
+
+    @Override
+    @CacheEvict(value = "labelsCache", allEntries = true)
+    <S extends LabelEntity> S saveAndFlush(S entity);
+
+    @Override
     @Query("SELECT DISTINCT l from LabelEntity l LEFT JOIN FETCH l.posts")
+    @Cacheable("labelsCache")
     List<LabelEntity> findAll();
 }
